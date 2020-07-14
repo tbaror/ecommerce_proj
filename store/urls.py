63729c3d101028a4b4fcc5639fc0a72a8d7fb9fb
tal_bar_or <tbaror@gmail.com>
2020-07-14 22:31:35 +0300

    set product detail page

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.aboutPage, name='about'),
+    path('product/', views.productPage, name='product'),
 ]
