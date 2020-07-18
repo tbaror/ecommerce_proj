cb2758a4eeab89da3852897f20ef9912b2a46dd7
tal_bar_or <tbaror@gmail.com>
2020-07-18 22:35:41 +0300

    slug view issue

--- store/models.py
+++ store/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    description = models.CharField(max_length=250, blank=True)
+    image = models.ImageField(upload_to='catogery', blank=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'catogery'
+        verbose_name_plural = 'catogeries'
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    description = models.CharField(max_length=250, blank=True)
+    catogery = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='catogery', blank=True)
+    stock = models.IntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+
+    def __str__(self):
+        return self.name
+
+            
--- store/views.py
+++ store/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from.models import Category, Product 
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'home.html')
+def home(request, category_slug=None):
+    category_page = None
+    products = None
+    if category_slug != None:
+        category_page = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=category_page, available=True)
+    else:
+        products = Product.objects.all().filter(available=True)
+
+    return render(request, 'home.html', {'category': category_page, 'products': products})
 
 def productPage(request):
     return render(request, 'product.html')    
