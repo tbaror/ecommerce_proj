8cf2691b754cb075b11471e31ba0ba0058f58af8
tal_bar_or <tbaror@gmail.com>
2020-08-08 23:04:08 +0300

    fix category

--- store/models.py
+++ store/models.py
@@ -1,16 +1,20 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=250, unique=True)
-    description = models.CharField(max_length=250, blank=True)
-    image = models.ImageField(upload_to='catogery', blank=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='category', blank=True)
 
     class Meta:
-        ordering = ['name']
-        verbose_name = 'catogery'
-        verbose_name_plural = 'catogeries'
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def get_url(self):
+        return reverse('products_by_category', args=[self.slug])
 
     def __str__(self):
         return self.name
@@ -18,17 +22,16 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=250, unique=True)
-    description = models.CharField(max_length=250, blank=True)
-    catogery = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='catogery', blank=True)
+    image = models.ImageField(upload_to='product', blank=True)
     stock = models.IntegerField()
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-
     class Meta:
-        ordering = ['name']
+        ordering = ('name',)
         verbose_name = 'product'
         verbose_name_plural = 'products'
 
