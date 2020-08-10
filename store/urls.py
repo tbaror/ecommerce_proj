45ea7e178bd3980207c0ce4a1a40ea8fed4610a4
tal_bar_or <tbaror@gmail.com>
2020-08-10 21:25:12 +0300

    product page and navbar listing

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('<slug:category_slug>', views.home, name='products_by_category'),
-    path('product/', views.productPage, name='product'),
+    path('category/<slug:category_slug>', views.home, name='products_by_category'),
+    path('category/<slug:category_slug>/<slug:product_slug>', views.productPage, name='product_detail'),
 ]
