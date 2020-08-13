57e18e9230c2ba50a98d0dd7b54abbd27609fbe5
tbaror <tbaror@gmail.com>
2020-08-13 18:24:24 +0300

    cart model

--- store/models.py
+++ store/models.py
@@ -41,4 +41,32 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+class Cart(models.Model):
+    cart_id = models.CharField(max_length=250, blank=True)
+    date_added = models.DateField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'Cart'
+        ordering = ['date_added']
+    
+
+    def __str__(self):
+        return self.cart_id
+
+class CartItem(models.Model):
+    Product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'CartItem'
+
+    def sub_total(self):
+        return self.price * self.quantity
+
+    
+
+    def __str__(self):
+        return self.product
             
