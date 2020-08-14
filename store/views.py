2b773324fa5fce58cb10853e007e0d186ee31cf9
tal_bar_or <tbaror@gmail.com>
2020-08-14 14:34:39 +0300

    cart logic

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
-from.models import Category, Product, Cart, CartItem 
+from.models import Category, Product, Cart, CartItem
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 
@@ -34,3 +35,33 @@ def add_cart(request, product_id):
     product = Product.objects.get(id=product_id)
     try:
         cart = Cart.objects.get(cart_id=_card_id(request))
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(
+            cart_id = _card_id(request)
+        )
+        cart.save()
+
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            product = product,
+            quantity =1,
+            cart = cart
+        )
+
+        cart_item.save()
+    return redirect('cart_detail')
+
+def cart_detail(request, total=0, counter=0, cart_items = None):
+    try:
+        cart = Cart.objects.get(cart_id= _card_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            counter += cart_item.quantity
+    except ObjectDoesNotExist:
+        pass
+    return render(request, 'cart.html', dict(cart_items=cart_items, total=total, counter=counter))        
